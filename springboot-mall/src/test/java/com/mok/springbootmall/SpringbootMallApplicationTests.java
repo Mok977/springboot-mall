8440fa0b67cc04b6982367c4bda1caeeb45dd622
Mok977 <smile977mok@gmail.com>
2023-06-05 19:23:46 +0800

    初始化 Spring boot project

---
+++ springboot-mall/src/test/java/com/mok/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mok.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
