301e70e57572b537db15bc98de7e06b78f7c3641
Su-Han Mo <smile977mok@gmail.com>
2023-06-23 21:27:28 +0800

    商品功能 - 實作新增商品

--- springboot-mall/src/main/java/com/mok/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/mok/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.mok.springbootmall.dao;
 
+import com.mok.springbootmall.dto.ProductRequest;
 import com.mok.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/mok/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/mok/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.mok.springbootmall.service;
 
+import com.mok.springbootmall.dto.ProductRequest;
 import com.mok.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
